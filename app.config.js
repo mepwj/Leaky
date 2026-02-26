b819dcf2dc6f6d25fbd46168078cc3eb2e2ec30d
mepwj <hoholeo382@gmail.com>
2026-02-26 14:21:43 +0900

    chore: Expo 설정 분리 및 빌드 경고 제거

---
+++ app.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Leaky',
+  slug: 'leaky',
+  version: '1.0.0',
+  scheme: 'leaky',
+  web: {
+    bundler: 'metro',
+    output: 'single',
+    title: 'Leaky',
+  },
+  plugins: ['expo-font'],
+};
